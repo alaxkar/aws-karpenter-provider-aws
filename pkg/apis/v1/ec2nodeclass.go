maxcao13/karpenter-provider-aws

UPSTREAM: <carry>: ignore drift on unstructured ignition userdata token field
We need to specially hash EC2NodeClass custom userData.
This is because the karpenter-operator passes a rotating token to the EC2NodeClass userData field as part of the payload.
This field unintentionally causes the hash to change and trigger drift.

This happens by parsing a special header we include in ignition-server requests, and only returning that header TargetConfigVersionHash's value.
The hash is unique and will act as a trigger for Drift rollout, similar to it's usage in HyperShift and the hyperv1.NodePool API.
https://github.com/openshift/hypershift/blob/07b5bf9a97d23d6c7a01164a385e5b9d6c513794/hypershift-operator/controllers/nodepool/config.go#L120

If the config was not a valid ignition config, then we bypass this special handling entirely.

Signed-off-by: Max Cao <macao@redhat.com>

pkg/apis/v1/ec2nodeclass.go
@@ -15,6 +15,7 @@ limitations under the License.
 package v1
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"strings"
@@ -488,8 +489,10 @@ type EC2NodeClass struct {
 const EC2NodeClassHashVersion = "v4"
 
 func (in *EC2NodeClass) Hash() string {
+	spec := in.Spec
+	spec.UserData = lo.ToPtr(in.UserDataHash())
 	return fmt.Sprint(lo.Must(hashstructure.Hash([]interface{}{
-		in.Spec,
+		spec,
 		// AMIFamily should be hashed using the dynamically resolved value rather than the literal value of the field.
 		// This ensures that scenarios such as changing the field from nil to AL2023 with the alias "al2023@latest"
 		// doesn't trigger drift.
@@ -602,6 +605,44 @@ func amiVersionFromAlias(alias string) string {
 		log.Fatalf("failed to parse AMI alias %q, invalid format", alias)
 	}
 	return components[1]
+}
+
+// UPSTREAM: <carry>: We need to specially hash our custom user data because we pass in a rotating token into the userData field
+// which unintentionally causes the hash to change and trigger drift. We specially handle any ignition userData by parsing it,
+// getting a special header we include in ignition server requests, and only return that header TargetConfigVersionHash's value.
+// The hash is unique and will act as a trigger for Drift rollout, similar to it's usage in HyperShift and the hyperv1.NodePool API.
+// https://github.com/openshift/hypershift/blob/07b5bf9a97d23d6c7a01164a385e5b9d6c513794/hypershift-operator/controllers/nodepool/config.go#L120
+//
+// Returns the TargetConfigVersionHash value from the userData's ignition payload, assuming it's a valid config.
+// If not valid, returns the raw userData, effectively bypassing this handling.
+func (in *EC2NodeClass) UserDataHash() string {
+	var ignitionConfig struct {
+		Ignition struct {
+			Config struct {
+				Merge []struct {
+					HTTPHeaders []struct {
+						Name  string  `json:"name"`
+						Value *string `json:"value"`
+					} `json:"httpHeaders"`
+				} `json:"merge"`
+			} `json:"config"`
+		} `json:"ignition"`
+	}
+
+	if err := json.Unmarshal([]byte(*in.Spec.UserData), &ignitionConfig); err != nil {
+		return *in.Spec.UserData
+	}
+
+	if len(ignitionConfig.Ignition.Config.Merge) == 0 {
+		return *in.Spec.UserData
+	}
+
+	for _, header := range ignitionConfig.Ignition.Config.Merge[0].HTTPHeaders {
+		if header.Name == "TargetConfigVersionHash" && header.Value != nil {
+			return *header.Value
+		}
+	}
+	return *in.Spec.UserData
 }
 
 // EC2NodeClassList contains a list of EC2NodeClass

alaxkar/aws-karpenter-provider-aws

pkg/apis/v1/ec2nodeclass.go
@@ -15,6 +15,7 @@ limitations under the License.
 package v1
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"strings"
@@ -588,15 +589,19 @@ type EC2NodeClass struct {
 	Status EC2NodeClassStatus `json:"status,omitempty"`
 }
 
+// TODO(maxcao13): if we ever change any hashes downstream, we will have to bump this version ourselves, irrespective of upstream.
+
 // We need to bump the EC2NodeClassHashVersion when we make an update to the EC2NodeClass CRD under these conditions:
 // 1. A field changes its default value for an existing field that is already hashed
 // 2. A field is added to the hash calculation with an already-set value
 // 3. A field is removed from the hash calculations
 const EC2NodeClassHashVersion = "v5"
 
-func (in *EC2NodeClass) Hash(caBundle *string) string {
-	return fmt.Sprint(lo.Must(hashstructure.Hash([]any{
-		in.Spec,
+func (in *EC2NodeClass) Hash() string {
+	spec := in.Spec
+	spec.UserData = in.UserDataHash()
+	return fmt.Sprint(lo.Must(hashstructure.Hash([]interface{}{
+		spec,
 		// AMIFamily should be hashed using the dynamically resolved value rather than the literal value of the field.
 		// This ensures that scenarios such as changing the field from nil to AL2023 with the alias "al2023@latest"
 		// doesn't trigger drift.
@@ -727,6 +732,50 @@ func amiVersionFromAlias(alias string) string {
 		log.Fatalf("failed to parse AMI alias %q, invalid format", alias)
 	}
 	return components[1]
+}
+
+// UPSTREAM: <carry>: We need to specially hash our custom user data because we pass in a rotating token into the userData field
+// which unintentionally causes the hash to change and trigger drift. We specially handle any ignition userData by parsing it,
+// getting a special header we include in ignition server requests, and only return that header TargetConfigVersionHash's value.
+// The hash is unique and will act as a trigger for Drift rollout, similar to it's usage in HyperShift and the hyperv1.NodePool API.
+// https://github.com/openshift/hypershift/blob/07b5bf9a97d23d6c7a01164a385e5b9d6c513794/hypershift-operator/controllers/nodepool/config.go#L120
+//
+// comes from https://github.com/openshift/hypershift/blob/c6c65ef3a26243489477c984af93655a33c4167b/hypershift-operator/controllers/nodepool/token.go#L426
+const TargetConfigVersionHashHeader = "TargetConfigVersionHash"
+
+// Returns the TargetConfigVersionHash value from the userData's ignition payload, assuming it's a valid config.
+// If not valid, returns the raw userData, effectively bypassing this handling.
+func (in *EC2NodeClass) UserDataHash() *string {
+	if in.Spec.UserData == nil {
+		return nil
+	}
+	var ignitionConfig struct {
+		Ignition struct {
+			Config struct {
+				Merge []struct {
+					HTTPHeaders []struct {
+						Name  string  `json:"name"`
+						Value *string `json:"value"`
+					} `json:"httpHeaders"`
+				} `json:"merge"`
+			} `json:"config"`
+		} `json:"ignition"`
+	}
+
+	if err := json.Unmarshal([]byte(*in.Spec.UserData), &ignitionConfig); err != nil {
+		return in.Spec.UserData
+	}
+
+	if len(ignitionConfig.Ignition.Config.Merge) == 0 {
+		return in.Spec.UserData
+	}
+
+	for _, header := range ignitionConfig.Ignition.Config.Merge[0].HTTPHeaders {
+		if header.Name == TargetConfigVersionHashHeader && header.Value != nil {
+			return header.Value
+		}
+	}
+	return in.Spec.UserData
 }
 
 // EC2NodeClassList contains a list of EC2NodeClass